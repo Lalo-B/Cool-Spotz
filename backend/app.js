f959631d711f7d544d9ae73ebadd93c00c3c505d
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2024-05-08 16:11:33 -0700

    Add error-handling middleware for common errors and debugging. pt 2 done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
-
+const { ValidationError } = require('sequelize');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express();
@@ -40,6 +40,53 @@ if (!isProduction) {
       }
     })
   );
-  
+
 app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+/*
+If the error that caused this error-handler to be called is an instance of
+ValidationError from the sequelize package, then the error was created from a
+Sequelize database validation error and the additional keys of title string and
+an object representation of its errors array and passed into the next error
+handling middleware.
+*/
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+/*
+This error handler is for formatting all the errors before returning a JSON
+response. It will include the error message, the error messages as a JSON
+object with key-value pairs, and the error stack trace (if the environment is
+  in development) with the status code of the error message.
+*/
+
+
   module.exports = app;
