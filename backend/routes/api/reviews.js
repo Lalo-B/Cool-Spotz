121d8e5a5aa2e4bfd98bfcf82a6ccdfbeec8cac8
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2024-05-17 09:30:49 -0700

    renamed files

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -4,8 +4,39 @@ const { Review } = require('../../db/models');
 
 const router = express.Router();
 
+//get all reviews by a spots id
+router.get('/:spotId', async (req,res)=>{
+    const reviews = await Review.findAll({
+        where: {
+            id: req.query.spotId
+        }
+    });
+    if(!reviews){
+        res.status(404);
+        res.header('content-type', 'application/json');
+        res.body({ messgae: "Spot couldn't be found"});
+        return res.json(res.body);
+    };
+    return res.json(reviews);
+});
+
+router.post('/:spotId', async (req,res)=>{
+    const body = req.body;
+    const newReview = Review.create({
+        body
+    })
+})
+
+
+//get all reviews of current user
 router.get('/', async (req,res)=>{
-    //
+    const user = req.user;
+    const reviews = await Review.findAll({
+        where: {
+            userId: user.id
+        }
+    });
+    res.json(reviews)
 });
 
 
